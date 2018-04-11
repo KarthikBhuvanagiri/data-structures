911918c16e75cbd1b4be58d7feaf7ebf079c66d1
Karthik Bhuvanagiri <karthik.bhuvanagiri@kony.com>
2018-04-11 14:46:01 +0530

    Moved stack test case into kar.ds.stack.test package

--- Datastructures/tests/kar/ds/stack/test/StackTest.java
+++ Datastructures/tests/kar/ds/stack/test/StackTest.java
@@ -1,4 +1,4 @@
-package kar.ds.linkedlist.test;
+package kar.ds.stack.test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
