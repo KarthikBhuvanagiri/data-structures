23c0aa2956b610110ee53960455fbaead01198c7
Karthik Bhuvanagiri <karthik.bhuvanagiri@kony.com>
2018-05-11 06:15:43 +0530

    Made Stack as generic type

--- Datastructures/src/kar/ds/stack/Stack.java
+++ Datastructures/src/kar/ds/stack/Stack.java
@@ -2,47 +2,47 @@ package kar.ds.stack;
 
 import kar.ds.linkedlist.SingleLinkedList;
 
-public class Stack extends SingleLinkedList {
+public class Stack<T> extends SingleLinkedList<T> {
 	
-	public void push(Object data) {
+	public void push(T data) {
 		super.insert(data);
 	}
 	
-	public Object pop() {
+	public T pop() {
 		return super.deleteAt(getSize() - 1);
 	}
 	
-	public Object peek() {
+	public T peek() {
 		return super.getDataAt(getSize() - 1);
 	}
 
 	@Override
-	public void insert(Object data) {
+	public void insert(T data) {
 		throw new StackException("Cannot insert into the stack!");
 	}
 	
 	@Override
-	public boolean insertAt(int index, Object data) {
+	public boolean insertAt(int index, T data) {
 		throw new StackException("Cannot insert at random location in the stack!");
 	}
 
 	@Override
-	public boolean update(Object data, int index) {
+	public boolean update(T data, int index) {
 		throw new StackException("Cannot update at random location in the stack!");
 	}
 
 	@Override
-	public boolean delete(Object data) {
+	public boolean delete(T data) {
 		throw new StackException("Cannot delete element at random location in the stack!");
 	}
 
 	@Override
-	public Object deleteAt(int index) {
+	public T deleteAt(int index) {
 		throw new StackException("Cannot delete at random location in the stack!");
 	}
 
 	@Override
-	public Object getDataAt(int index) {
+	public T getDataAt(int index) {
 		throw new StackException("Cannot get element at random location in the stack!");
 	}
 
--- Datastructures/tests/kar/ds/stack/test/StackTest.java
+++ Datastructures/tests/kar/ds/stack/test/StackTest.java
@@ -12,11 +12,11 @@ import kar.ds.stack.StackException;
 
 class StackTest {
 
-	void assertSizeAndContent(int expectedSize, Object[] expectedContent, Stack stack) {
+	void assertSizeAndContent(int expectedSize, Integer[] expectedContent, Stack<Integer> stack) {
 		int size = stack.getSize();
 		assertEquals(expectedSize, size);
 
-		Object[] actualContent = stack.traverse();
+		Integer[] actualContent = stack.traverse();
 		if(expectedContent != null)
 			assertArrayEquals(expectedContent, actualContent);
 		else
@@ -25,22 +25,22 @@ class StackTest {
 	
 	@Test
 	void testEmptyStackCreation() {
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		assertSizeAndContent(0, null, stack);
 	}
 	
 	@Test
 	void testPushOneItem() {
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		stack.push(0);
 		
-		assertSizeAndContent(1, new Object[] {0}, stack);
+		assertSizeAndContent(1, new Integer[] {0}, stack);
 	}
 	
 	@Test
 	void testPushThreeItems() {
 		Integer[] content = new Integer[] {0,1,2};
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		for(Integer i : content) {
 			stack.push(content[i]);
 		}
@@ -50,8 +50,8 @@ class StackTest {
 	
 	@Test
 	void testPopEmptyStack() {
-		Stack stack = new Stack();
-		Object data = stack.pop();
+		Stack<Integer> stack = new Stack<Integer>();
+		Integer data = stack.pop();
 		
 		assertNull(data);
 		assertSizeAndContent(0, null, stack);
@@ -60,29 +60,29 @@ class StackTest {
 	@Test
 	void testPopNonEmptyStack() {
 		Integer[] content = new Integer[] {0,1,2};
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		for(Integer i : content) {
 			stack.push(content[i]);
 		}
-		Object data = stack.pop();
+		Integer data = stack.pop();
 		
-		assertEquals(2, data);
-		assertSizeAndContent(2, new Object[] {0,1}, stack);
+		assertEquals(Integer.valueOf(2), data);
+		assertSizeAndContent(2, new Integer[] {0,1}, stack);
 	}
 	
 	@Test
 	void testPopFromStackHavingOnlyItem() {
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		stack.push(0);
-		Object data = stack.pop();
+		Integer data = stack.pop();
 		
-		assertEquals(0, data);
+		assertEquals(Integer.valueOf(0), data);
 		assertSizeAndContent(0, null, stack);
 	}
 	
 	@Test
 	void testEmptyStackByPoppingAllItems() {
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		stack.push(0);
 		stack.push(1);
 		stack.push(2);
@@ -91,13 +91,13 @@ class StackTest {
 		stack.pop();
 		stack.pop();
 		
-		assertSizeAndContent(0, null, stack);
+		assertSizeAndContent(Integer.valueOf(0), null, stack);
 	}
 	
 	@Test
 	void testPeekEmptyStack() {
-		Stack stack = new Stack();
-		Object data = stack.peek();
+		Stack<Integer> stack = new Stack<Integer>();
+		Integer data = stack.peek();
 		
 		assertNull(data);
 		assertSizeAndContent(0, null, stack);
@@ -106,59 +106,59 @@ class StackTest {
 	@Test
 	void testPeekNOnEmptyStack() {
 		Integer[] content = new Integer[] {0,1,2};
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		for(Integer i : content) {
 			stack.push(content[i]);
 		}
-		Object data = stack.peek();
+		Integer data = stack.peek();
 		
-		assertEquals(2, data);
+		assertEquals(Integer.valueOf(2), data);
 		assertSizeAndContent(3, content, stack);
 	}
 	
 	@Test
 	void testPeekStackHavingOnlyItem() {
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		stack.push(0);
-		Object data = stack.peek();
+		Integer data = stack.peek();
 		
-		assertEquals(0, data);
-		assertSizeAndContent(1, new Object[] {0}, stack);
+		assertEquals(Integer.valueOf(0), data);
+		assertSizeAndContent(1, new Integer[] {0}, stack);
 	}
 	
 	@Test
 	void testInsert(){
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		assertThrows(StackException.class, ()->{stack.insert(0);});
 	}
 	
 	@Test
 	void testInsertAt(){
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		assertThrows(StackException.class, ()->{stack.insertAt(0, 0);});
 	}
 	
 	@Test
 	void testDelete(){
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		assertThrows(StackException.class, ()->{stack.delete(0);});
 	}
 	
 	@Test
 	void testDeleteAt(){
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		assertThrows(StackException.class, ()->{stack.deleteAt(0);});
 	}
 	
 	@Test
 	void testUpdate(){
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		assertThrows(StackException.class, ()->{stack.update(0,0);});
 	}
 	
 	@Test
 	void testGetDataAt(){
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		assertThrows(StackException.class, ()->{stack.getDataAt(0);});
 	}
 
