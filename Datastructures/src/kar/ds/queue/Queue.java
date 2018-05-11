e818cbdbfa42e07bc54e741f34b7fed06e99e926
Karthik Bhuvanagiri <karthik.bhuvanagiri@kony.com>
2018-05-11 06:10:53 +0530

    Made Queue as generic type

--- Datastructures/src/kar/ds/queue/Queue.java
+++ Datastructures/src/kar/ds/queue/Queue.java
@@ -3,47 +3,47 @@ package kar.ds.queue;
 import kar.ds.linkedlist.SingleLinkedList;
 import kar.ds.queue.QueueException;
 
-public class Queue extends SingleLinkedList {
+public class Queue<T> extends SingleLinkedList<T> {
 
-	public void enqueue(Object data) {
+	public void enqueue(T data) {
 		super.insert(data);
 	}
 	
-	public Object dequeue() {
+	public T dequeue() {
 		return super.deleteAt(0);
 	}
 	
-	public Object peek() {
+	public T peek() {
 		return super.getDataAt(0);
 	}
 	
 	@Override
-	public void insert(Object data) {
+	public void insert(T data) {
 		throw new QueueException("Cannot insert into the queue!");
 	}
 	
 	@Override
-	public boolean insertAt(int index, Object data) {
+	public boolean insertAt(int index, T data) {
 		throw new QueueException("Cannot insert at random location in the queue!");
 	}
 
 	@Override
-	public boolean update(Object data, int index) {
+	public boolean update(T data, int index) {
 		throw new QueueException("Cannot update at random location in the queue!");
 	}
 
 	@Override
-	public boolean delete(Object data) {
+	public boolean delete(T data) {
 		throw new QueueException("Cannot delete element at random location in the queue!");
 	}
 
 	@Override
-	public Object deleteAt(int index) {
+	public T deleteAt(int index) {
 		throw new QueueException("Cannot delete at random location in the queue!");
 	}
 
 	@Override
-	public Object getDataAt(int index) {
+	public T getDataAt(int index) {
 		throw new QueueException("Cannot get element at random location in the queue!");
 	}
 }
--- Datastructures/tests/kar/ds/queue/test/QueueTest.java
+++ Datastructures/tests/kar/ds/queue/test/QueueTest.java
@@ -12,11 +12,11 @@ import kar.ds.queue.QueueException;
 
 class QueueTest {
 
-	void assertSizeAndContent(int expectedSize, Object[] expectedContent, Queue queue) {
+	void assertSizeAndContent(int expectedSize, Integer[] expectedContent, Queue<Integer> queue) {
 		int size = queue.getSize();
 		assertEquals(expectedSize, size);
 
-		Object[] actualContent = queue.traverse();
+		Integer[] actualContent = queue.traverse();
 		if(expectedContent != null)
 			assertArrayEquals(expectedContent, actualContent);
 		else
@@ -25,22 +25,22 @@ class QueueTest {
 	
 	@Test
 	void testEmptyQueueCreation() {
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		assertSizeAndContent(0, null, queue);
 	}
 	
 	@Test
 	void testEnqueueOneItem() {
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		queue.enqueue(0);
 		
-		assertSizeAndContent(1, new Object[] {0}, queue);
+		assertSizeAndContent(1, new Integer[] {0}, queue);
 	}
 	
 	@Test
 	void testEnqueueThreeItems() {
 		Integer[] content = new Integer[] {0,1,2};
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		for(Integer i : content) {
 			queue.enqueue(content[i]);
 		}
@@ -50,8 +50,8 @@ class QueueTest {
 	
 	@Test
 	void testDequeueEmptyQueue() {
-		Queue queue = new Queue();
-		Object data = queue.dequeue();
+		Queue<Integer> queue = new Queue<Integer>();
+		Integer data = queue.dequeue();
 		
 		assertNull(data);
 		assertSizeAndContent(0, null, queue);
@@ -60,29 +60,29 @@ class QueueTest {
 	@Test
 	void testDequeueNonEmptyQueue() {
 		Integer[] content = new Integer[] {0,1,2};
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		for(Integer i : content) {
 			queue.enqueue(content[i]);
 		}
-		Object data = queue.dequeue();
+		Integer data = queue.dequeue();
 		
-		assertEquals(0, data);
-		assertSizeAndContent(2, new Object[] {1,2}, queue);
+		assertEquals(Integer.valueOf(0), data);
+		assertSizeAndContent(2, new Integer[] {1,2}, queue);
 	}
 	
 	@Test
 	void testDequeueFromQueueHavingOnlyItem() {
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		queue.enqueue(0);
-		Object data = queue.dequeue();
+		Integer data = queue.dequeue();
 		
-		assertEquals(0, data);
+		assertEquals(Integer.valueOf(0), data);
 		assertSizeAndContent(0, null, queue);
 	}
 	
 	@Test
 	void testEmptyQueueByDequeingAllItems() {
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		queue.enqueue(0);
 		queue.enqueue(1);
 		queue.enqueue(2);
@@ -96,8 +96,8 @@ class QueueTest {
 	
 	@Test
 	void testPeekEmptyQueue() {
-		Queue queue = new Queue();
-		Object data = queue.peek();
+		Queue<Integer> queue = new Queue<Integer>();
+		Integer data = queue.peek();
 		
 		assertNull(data);
 		assertSizeAndContent(0, null, queue);
@@ -106,59 +106,59 @@ class QueueTest {
 	@Test
 	void testPeekNOnEmptyQueue() {
 		Integer[] content = new Integer[] {0,1,2};
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		for(Integer i : content) {
 			queue.enqueue(content[i]);
 		}
-		Object data = queue.peek();
+		Integer data = queue.peek();
 		
-		assertEquals(0, data);
+		assertEquals(Integer.valueOf(0), data);
 		assertSizeAndContent(3, content, queue);
 	}
 	
 	@Test
 	void testPeekQueueHavingOnlyItem() {
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		queue.enqueue(0);
-		Object data = queue.peek();
+		Integer data = queue.peek();
 		
-		assertEquals(0, data);
-		assertSizeAndContent(1, new Object[] {0}, queue);
+		assertEquals(Integer.valueOf(0), data);
+		assertSizeAndContent(1, new Integer[] {0}, queue);
 	}
 	
 	@Test
 	void testInsert(){
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		assertThrows(QueueException.class, ()->{queue.insert(0);});
 	}
 	
 	@Test
 	void testInsertAt(){
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		assertThrows(QueueException.class, ()->{queue.insertAt(0, 0);});
 	}
 	
 	@Test
 	void testDelete(){
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		assertThrows(QueueException.class, ()->{queue.delete(0);});
 	}
 	
 	@Test
 	void testDeleteAt(){
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		assertThrows(QueueException.class, ()->{queue.deleteAt(0);});
 	}
 	
 	@Test
 	void testUpdate(){
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		assertThrows(QueueException.class, ()->{queue.update(0,0);});
 	}
 	
 	@Test
 	void testGetDataAt(){
-		Queue queue = new Queue();
+		Queue<Integer> queue = new Queue<Integer>();
 		assertThrows(QueueException.class, ()->{queue.getDataAt(0);});
 	}
 
